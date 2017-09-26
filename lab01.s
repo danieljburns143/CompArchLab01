4d1696824a3186a9ab5382e8a8c0d14143c26104
cgreen14 <cgreen14@nd.edu>
2017-09-26 13:47:04 -0400

    maybe

--- lab01.s
+++ lab01.s
@@ -37,8 +37,13 @@ main:		ldr 	r3, =my_array	@ Load the starting address of the first
 
 mov r9, #0 @make a i to count iterations of loop
 add r7, r7, #4
-mov r1, #-1
-add r5, r5, #32
+mov r2, #-1
+@Make -1 Node at start, and iterate r5 to r5 + 32 (start of list)
+str r5, [r5]        @set prev pointer to its location
+str r2, [r5, #8]   @set value of start node to -1
+mov r1, r5         @save value of start
+add r5, r5, #32   @iterate to the true node 1
+str r5, [r1, #4]  @place location of node 1 in the next pointer of the start node
 
 loadLoop:				@ You should create a loop that cycles through all
                                     	@ of the array elements to create a doubly-linked list
@@ -64,10 +69,8 @@ loadLoop:				@ You should create a loop that cycles through all
                                 			@to the head of the list; you may use the value -1 as we will not test your code with any negative numbers
 
 exitLoad:
-  mov r2, #0
 	mov r1, #-1
   add r5, r3, #400  @set r5 to the address of the start of the linked list
-	str r1, [r5]     @set prev pointer on n = 1 to -1
   str r1, [r6, #4]     @set next pointer on n = 20 to -1
 
 @##########
@@ -108,7 +111,11 @@ sort:
     @ j is now r7 and j - 1 is now r9
     cmp r7, r9
     bge postWhi  @go to next loop if as j and j-1 are in order
+    sub r13, r13, #4	@ make space for r2 to be saved in the stack
+    str r2, [r13]
+    mov r14,r15
     b swap
+    ldr r2, [r13] @pull r2 from the stack
     sub r2, r2, #1		@ j-1 as we move closer to zero
 		b	whiLoop			@ go back to start of while loop
 
@@ -162,14 +169,23 @@ swap:
         @ write a function to swap n and n-1
         @ assume r5 is the address of n
 
-        ldr r2, [r5] @ load address of N-1
-        ldr r6, [r2] @ load previous of N-1 (address to n-2)
-        str r5, [r2] @ store address of n as previous of n - 1
-      	str r6, [r5] @ store address prev n-1 into as prev of n
-      	ldr r6, [r2, #4] @ r6 is now the next of i - 1
-      	ldr r7, [r5, #4] @ store the next of n as r7
-        str r6, [r5, #4] @set new next pointer for n as the address of the value of n
-        str r7, [r2, #4] @set new next pointer fro n-1 as next of n
+        ldr r2, [r5]  @load address of n-1
+        ldr r6, [r8]  @load address of n - 2
+        str r5, [r6, #4]  @send next of n-2 to point to address of n
+
+        ldr r6, [r5, #4] @load address of n+1 (next pointer of n)
+        str r2, [r6]  @send the prev pointer in n+1 to point to n-1
+
+        @load the next pointer of n
+        @load the next pointer of n - 1
+        str r6, [r2, #4] @set the next pointer of n-1 to the next of n
+        str r2, [r5, #4] @set the next pointer of n to the the addr of n
+
+        ldr r6, [r2]  @load the prev pointer of n-1
+        @load the prev pointer of n (which is addr of n-1)
+        str r6, [r5]  @set the prev pointer of n to the prev pointer of n -1, which is the addr of 2
+        str r5, [r2]  @set the prev pointer of n-1 to the addr of n
+
 
         mov r15,r14
 
@@ -177,12 +193,10 @@ swap:
 @# DELETE FUNCTION -- you must write
 @###########################################
 delete:
-					@save next value of node being deleted
-					@go back to previous node and update next value to register just used
-					@save location of current register
-					@go to next new next node
-					@update previous value of current register, to the node we just left
-
+					@you want to delete the node located at r6
+          @get address of n-1, and n+1
+          @set prev pointer of n+1 to n-1
+          @set next pointer of n-1 to the addr of n+1
 
 
 @###########################################
